94a2dfb9998b2f32cbad3b57133f2d916c623efa
Guilherme da Silva <dprogramador731@gmail.com>
2026-07-15 20:06:59 -0300

    Codigo limpo

--- admin.js
+++ admin.js
@@ -1,75 +1,68 @@
 const SUPABASE_URL = "https://ppmhkjxhqtldaimlwjbx.supabase.co";
 const SUPABASE_KEY = "sb_publishable_V7wts_Jpiq6RgaDbjaBPrg_zUgwqfo1";
 
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
-
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const listaAgendamentos = document.getElementById("listaAgendamentos");
 
-
 async function carregarAgendamentos() {
-
-    const { data, error } = await supabaseClient
+  const { data, error } = await supabaseClient
     .from("agendamentos")
-    .select(`
+    .select(
+      `
         *,
         clientes (
             nome,
             telefone,
             tipo_cliente
         )
-    `)
+    `,
+    )
     .order("data", { ascending: true });
 
-    if (error) {
-        console.log("Erro ao buscar agendamentos:", error);
-        listaAgendamentos.innerHTML = `
+  if (error) {
+    console.log("Erro ao buscar agendamentos:", error);
+    listaAgendamentos.innerHTML = `
             <p>Erro ao carregar agendamentos.</p>
         `;
-        return;
-    }
+    return;
+  }
 
+  console.log("Agendamentos:", data);
 
-    console.log("Agendamentos:", data);
-
-
-    if (data.length === 0) {
-        listaAgendamentos.innerHTML = `
+  if (data.length === 0) {
+    listaAgendamentos.innerHTML = `
             <p>Nenhum agendamento encontrado.</p>
         `;
-        return;
-    }
+    return;
+  }
 
-
-    listaAgendamentos.innerHTML = data.map(agendamento => {
-
-        return `
+  listaAgendamentos.innerHTML = data
+    .map((agendamento) => {
+      return `
         <div class="card-option">
+<strong>${agendamento.clientes.nome}</strong>
 
-            <strong>${agendamento.nome}</strong>
+<p>
+📞 ${agendamento.clientes.telefone}
+</p>
 
-            <p>
-            ✂️ ${agendamento.servico}
-            </p>
+<p>
+✂️ ${agendamento.servico}
+</p>
 
-            <p>
-            📅 ${agendamento.data}
-            </p>
+<p>
+📅 ${agendamento.data}
+</p>
 
-            <p>
-            ⏰ ${agendamento.horario}
-            </p>
+<p>
+⏰ ${agendamento.horario}
+</p>
 
         </div>
         `;
-
-    }).join("");
-
+    })
+    .join("");
 }
-
 
 carregarAgendamentos();
